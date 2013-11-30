ee49ebd9e9f2754eb68c4aa7dff20d3e1bd66e19
Joe Erickson <joe@jerickson.net>
2013-11-30 10:32:12 -0500

    Fill out the main index page a little more

--- index.php
+++ index.php
@@ -1,20 +1,28 @@
 <!DOCTYPE html>
-<html>
+<html <?php language_attributes() ?> >
 <head>
+  <meta charset="<?php bloginfo( 'charset' ) ?>" />
+  <meta name="viewport" content="width=device-width">
+  <meta name="description" content="<?php bloginfo( 'description' ) ?>" />
+  <link rel="pingback" href="<?php bloginfo( 'pingback_url' ) ?>" />
   <title><?php bloginfo('name') ?> <?php wp_title() ?></title>
   <link rel="stylesheet" type="text/css" 
     href="<?php echo get_stylesheet_uri() ?>" media="all">
   <?php wp_head() ?>
 </head>
-<body>
+<body <?php body_class() ?> >
   <div class="container">
     <header>
-      <h1>Welcome to <?php bloginfo('name') ?></h1>
+      <h1>
+        <a href="<?php echo home_url() ?>">
+          Welcome to <?php bloginfo('name') ?>
+        </a>
+      </h1>
       <h2><?php bloginfo('description') ?></h2>
     </header>
     <?php while ( have_posts() ) : the_post(); ?>
-      <article>
-        <h2><?php the_title() ?></h2>
+      <article <?php post_class()?> >
+        <h2><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h2>
         <?php the_content() ?>
       </article>
       <hr>
